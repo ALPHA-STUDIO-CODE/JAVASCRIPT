20b8712f20845f40db3735c5cbf4dafc1fa87284
Alpha <you@example.comalphayomide@gmail.com>
2026-05-02 16:04:03 +0100

    add HTML and JavaScript files for variable examples

---
+++ 02-variable/script.js
@@ -0,0 +1,23 @@
+// Use let keyword to store a data in a variable
+// The var keyword is a old method used for the but not longer advisable to use
+
+// add variable "firstName" and "lastName" // so that they equal your name
+let firstName = "Alpha";
+let lastName = "Studio";
+// create a variable that holds the answer // of "firstName" + " " + "lastName"
+let nameIs = firstName + " " + lastName;
+console.log(nameIs); // Alpha Studio
+
+// Evaluate this question. What is a + b?
+let a = 34;
+let b = 21;
+a = 2;
+console.log(a + b); // 23
+
+// What is c equal to?
+let c;
+console.log(c); // undefined
+
+//Prompt for introduction
+let userName = prompt("What is your name"); // Prompt user for their name
+alert("Welcome back " + userName + "!"); // Alert user with a welcome message
