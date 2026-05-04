81bac97ad63bd9d2b6e314747c600bbd63f152d0
Alpha <you@example.comalphayomide@gmail.com>
2026-05-04 19:12:07 +0100

    add HTML and JavaScript files for number examples

---
+++ 04-number/script.js
@@ -0,0 +1,37 @@
+// Basic arithmetic operation
+let addition = 10 + 5; // For addition
+let subtraction = 3 - 4; // For subtraction
+let multiplication = 5 * 4; // For multiplication
+let division = 6 / 2; // For division
+let modulo = 5 % 10; // For modulo : modulo is used to get remainder of a the division
+
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
+console.log(modulo);
+
+//Dog age in human years challenge
+let dogAge = prompt("What is the age of your dog");
+let humanAge = (dogAge - 2) * 4 + 21;
+alert("Your dog is " + humanAge + " years old in Human years");
+
+// Increment
+let x = 8;
+x++; // This is use to increase x by 1
+console.log(x);
+
+let p = 9;
+p += 3; // This is use to increase p by 3
+console.log(p);
+
+// Decrement
+let y = 8;
+y--; // This is use to decrease y by 1
+console.log(y);
+
+let q = 19;
+q -= 3; // This is use to decrease q by 3
+console.log(q);
+
+// *=  and /= also follow same principle
